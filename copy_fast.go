c9f58968474274b6115debbed071d417d129f1e2
dada <bg5sbk@gmail.com>
2016-01-05 21:51:42 +0800

    修改文件名错误

--- copy_fast.go
+++ copy_fast.go
@@ -1,9 +1,11 @@
-// +build !go1.5
+// +build go1.5
 
 package main
 
 import "io"
 
 func copy(dst io.WriteCloser, src io.ReadCloser) {
-	io.Copy(dst, src)
+	buf := bufferPool.Get().([]byte)
+	defer bufferPool.Put(buf)
+	io.CopyBuffer(dst, src, buf)
 }
--- copy_slow.go
+++ copy_slow.go
@@ -1,11 +1,9 @@
-// +build go1.5
+// +build !go1.5
 
 package main
 
 import "io"
 
 func copy(dst io.WriteCloser, src io.ReadCloser) {
-	buf := bufferPool.Get().([]byte)
-	defer bufferPool.Put(buf)
-	io.CopyBuffer(dst, src, buf)
+	io.Copy(dst, src)
 }
